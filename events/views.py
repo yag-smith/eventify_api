5d5a339c479ec76ce80c411259254b58fc3df161
yag-smith <yair.araujo@tecsup.edu.pe>
2025-10-15 21:57:48 -0500

    ViewSets para CRUD

--- events/views.py
+++ events/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
+from .models import Evento, Organizador
+from .serializers import EventoSerializer, OrganizadorSerializer
 
-# Create your views here.
+class OrganizadorViewSet(viewsets.ModelViewSet):
+    queryset = Organizador.objects.all()
+    serializer_class = OrganizadorSerializer
+
+
+class EventoViewSet(viewsets.ModelViewSet):
+    queryset = Evento.objects.all()
+    serializer_class = EventoSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['nombre', 'ubicacion']  # Permite búsqueda con ?search=
