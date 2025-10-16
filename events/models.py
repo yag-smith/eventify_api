fae5d3b3dd778f249776508ed91ee8c900a1dd6c
yag-smith <yair.araujo@tecsup.edu.pe>
2025-10-15 21:48:23 -0500

    add models

--- events/models.py
+++ events/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Organizador(models.Model):
+    nombre = models.CharField(max_length=100)
+    contacto = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Evento(models.Model):
+    nombre = models.CharField(max_length=100)
+    fecha = models.DateField()
+    ubicacion = models.CharField(max_length=150)
+    organizador = models.ForeignKey(Organizador, on_delete=models.CASCADE, related_name='eventos')
+
+    def __str__(self):
+        return self.nombre
